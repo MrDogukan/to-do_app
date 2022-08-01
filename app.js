9df9b896f5449dbb84d52ff3aebd3052efb0734e
aliseslikaya <aliseslikaya@gmail.com>
2022-08-01 14:15:03 +0300

    Last Commit

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
-const addForm = document.querySelector('.add');
-const list = document.querySelector('.todolist');
-const comItem = document.querySelector('.complete');
-const addToDo = document.querySelector(".add-to-do");
+const inputText=document.querySelector("#task");
+const button=document.querySelector(".add-btn");
+const form=document.querySelector("#add-task");
+const ul=document.querySelector(".todolist-list-container");
+
+// functions
+const createTask=(taskText)=>{
+    return `
+    <div class="list-group">
+                    <li class="unchecked">${taskText}</li>
+                    <button class="btn">&#128473;</button>
+                </div>
+                `
+}
+
+const completedTaskCount=()=>{
+    let checkedCount=ul.getElementsByClassName("checked").length;
+    let taskCount=ul.getElementsByClassName("list-group").length;
+    console.log(checkedCount,taskCount);
+    const checkedTasks=document.getElementById("checkedTasks");
+    const allTasks=document.getElementById("allTasks");
+    checkedTasks.innerText=checkedCount;
+    allTasks.innerText=taskCount;
+
+}
 
 
-const generateListe = todo => {
-  const html = `
-  <li class="list-item"><i class="far fa-check-circle complete"></i>
-  <span>${todo}</span><i class="far fa-trash-alt delete"></i></li>`;
-  list.innerHTML += html;
-};
-
-list.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
-  }
-});
-
-
-
-list.addEventListener('click', e => {
-  if (e.target.classList.contains("complete")) {
-    if ((e.target.parentElement).style.textDecoration != 'line-through')
-      (e.target.parentElement).style.textDecoration = 'line-through';
-    else (e.target.parentElement).style.textDecoration = 'none';
-  }
-});
-
-addForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const todo = addForm.add.value.trim();
-  if (todo.length) {
-    generateListe(todo);
-    addForm.reset();
-  }
-});
-
-addToDo.addEventListener('click', e => {
+//events
+//add task event
+button.addEventListener("click",e=>{
     e.preventDefault();
-    const todo = addForm.add.value.trim();
-    if (todo.length) {
-      generateListe(todo);
-      addForm.reset();
+    if(inputText.value){
+    ul.innerHTML+=createTask(inputText.value);
+    form.reset();
+    }else{
+        alert("Please select a task");
     }
-  });
+    completedTaskCount();
+})
+// delete task
+ul.addEventListener("click",e=>{
+    if(e.target.classList.contains("btn")){
+        e.target.parentElement.remove();
+    }
+    completedTaskCount();
+})
+
+//check & uncheck Task
+ul.addEventListener("click", e=>{
+    if(e.target.classList.contains("unchecked")){
+        e.target.className="checked";
+    }else{
+        e.target.className="unchecked";
+    }
+    completedTaskCount();
+})
